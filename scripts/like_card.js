dbdbdf616316f733ab3cb1e016b710fed4e8f01b
k33pup <nikoboga90@gmail.com>
2024-05-19 14:26:35 +0300

    review fix

--- scripts/like_card.js
+++
@@ -1,9 +0,0 @@
-const likeListen = (element) => {
-  element.addEventListener('click', (evt) => {
-    const card = evt.target;
-    card.classList.toggle('element__like_active')
-  })
-}
-
-const likeBttns = document.querySelectorAll('.element__like');
-likeBttns.forEach(likeListen);
